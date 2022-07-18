2d24e8ebd440a33291376e7ff299de1cef217fff
threebarber <dvtdropshot@gmail.com>
2022-07-18 13:42:54 -0400

    2nd commit - 3 functioning buttons and updating text

--- scripts.js
+++ scripts.js
@@ -1,17 +1,88 @@
-function userChoice(e){
-    const userChoiceText = document.querySelector('.user-choice');
-    
-    
-    userChoiceText.innerText = "You chose " + e.id;
-    }
-    
-    
-    function reset(){
+
+
+
+function resetDiv() {
     let userChoiceText = document.querySelector('.user-choice');
-    let compChoiceText = document.querySelector('.comp-choice');
+    /* let compChoiceText = document.querySelector('.comp-choice');*/
+
+
+    userChoiceText.innerText = "Play Rock Paper Scissors";
+    /* compChoiceText.innerText = " "; */
+
+}
+
+
+
+function updateDiv(text) {
+    const userChoiceText = document.querySelector('.user-choice');
+
+    userChoiceText.innerText = text;
+}
+
+
+function getCompChoice(){
+
+    let choices = ["rock","paper","scissors"];
+    
+    let ranNumber = Math.floor(Math.random() * choices.length);
+    
+    choice = choices[ranNumber];
     
     
-    userChoiceText.innerText = "Make your choice ";
-    compChoiceText.innerText = " ";
+    console.log("Computer chose: " +choice);
+    
+    
+    return choice;
+    }
+    
+
+
+function playGame(e){
+
+    let userChoice = e.id;
+    let compChoice = getCompChoice();
+
+
+    let winnerString = playRound(userChoice,compChoice)
+    console.log("Winner: "+winnerString);
+
+    let divUpdateText = "You Chose: " +userChoice+ "\nComputer chose: "+compChoice+"\nWinner: "+winnerString;
+    updateDiv(divUpdateText);
+}
+
+
+function playRound(userChoice, compChoice){
+
+    userChoice = userChoice.toLowerCase();
+    compChoice = compChoice.toLowerCase();
+    
+    let winnerString;
+    
+    
+    if (userChoice == compChoice){
+     winnerString = "tie";
+     return winnerString;
+    }
+    else if(userChoice == "rock" && compChoice == "paper"){
+     winnerString = "computer";
+     return winnerString;
+    }else if(userChoice == "rock" && compChoice == "scissors"){
+     winnerString = "user";
+     return winnerString;
+    }else if(userChoice == "paper" && compChoice == "rock"){
+     winnerString = "user";
+     return winnerString;
+    }else if(userChoice == "paper" && compChoice == "scissors"){
+     winnerString = "computer";
+     return winnerString;
+    }else if(userChoice == "scissors" && compChoice == "paper"){
+     winnerString = "user";
+     return winnerString;
+    }else if (userChoice == "scissors" && compChoice == "rock"){
+     winnerString = "computer";
+     return winnerString;
+    }
     
     }
+    
+
